547d9b0d162ddf4d0236567202cd8114468378a0
nmzhaojie <33836622+nmzhaojie@users.noreply.github.com>
2018-11-25 23:58:41 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,356 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+/* 以下为页面组件 */
+import Login from '../views/page/Login';	//登录页
+import Page from '../components/page/Page';	//主页面模板
+import Readme from '../views/page/Readme';	//了解系统
+import ResetPwd from '../views/page/ResetPwd';//重置密码
+import PersonalCenter from '../views/page/PersonalCenter';	//个人中心
+
+//业务组件
+	//表格组件
+	import Empty from '../views/page/empty.vue';
+	import Table from '../views/table/Table.vue';
+	import FixedTable from "../views/table/FixedTable.vue";
+	import Multistage from "../views/table/Multistage.vue";
+	import CheckTable from "../views/table/CheckTable.vue";
+	import SortTable from "../views/table/SortTable.vue";
+	import ExpandTable from "../views/table/ExpandTable.vue";
+	import AddUpTable from "../views/table/AddUpTable.vue";
+	import MergeTable from "../views/table/mergeTable.vue";
+
+	import ElTable from '../views/table/ElTable'; //eleUI表格组件
+	import DataSource from "../views/table/DataSource";	//组定义表格组件
+
+	//图表组件
+	import Schart from "../views/charts/Schart.vue";
+
+	//表单组件
+	import Baseform from "../views/form/baseform.vue";
+	import ExtendForm from "../views/form/extendform.vue";
+	import RegisterDone from "../views/form/registerDone.vue";
+	import FormTest from "../views/form/formTest.vue";
+
+	//上传组件
+	import BaseUpload from "../views/upload/BaseUpload.vue";//点击上传
+	import Picwall from "../views/upload/picwall.vue";
+	import Piclist from "../views/upload/piclist.vue";
+	import DragUpload from "../views/upload/dragUpload.vue";
+	import ClipUpload from "../views/upload/clipUpload.vue";
+
+	//消息通知组件
+	import Alert from "../views/notice/Alert.vue";
+	import Message from "../views/notice/Message.vue";
+	import Notification from "../views/notice/Notification.vue";
+	import Loading from "../views/notice/Loading.vue";
+	import Dialog from "../views/notice/Dialog.vue";
+	import Tip from "../views/notice/Tip.vue";
+	import Popover from "../views/notice/Popover.vue";
+
+	//工具类
+	import Tag from "../views/tools/Tag.vue";
+	import Badge from "../views/tools/Badge.vue";
+	import Pagination from "../views/tools/Pagination.vue";
+	import Progress from "../views/tools/Progress.vue";
+	import Step from "../views/tools/Step.vue";
+
+	//控制面板组件
+	import Collapse from "../views/panel/Collapse.vue";
+	import Card from "../views/panel/Card.vue";
+
+	//菜单类
+	import Dropdown from "../views/menu/Dropdown.vue";
+	import Navmenu from "../views/menu/Navmenu.vue";
+	import Tab from "../views/menu/Tab.vue";
+
+	//其它组件
+	import Drag from '../views/else/drag.vue';
+	import Tree from "../views/else/Tree.vue";
+	import Carousel from "../views/else/Carousel.vue";
+
+	//layout 布局
+	import Layout from "../views/layout/Layout.vue";
+	import Container from "../views/layout/Container.vue";
+
+	//全局介绍
+	import Font from "../views/overall/Font.vue";
+	import Color from "../views/overall/Color.vue";
+	import Icon from "../views/overall/Icon.vue";
+	import Button from "../views/overall/Button.vue";
+
+	import Demo from "../views/layout/Demo.vue";
+
+
+Vue.use(Router);
+
+export default new Router({
+	routes:[
+		{
+			path: '/',
+			redirect:'/login' //重定向
+		},
+		{
+			path:'/login',//登陆
+			//component: resolve => require(['../components/page/Login.vue'],resolve)
+			component: Login
+		},
+		{
+			path:'/demo',
+			component:Demo
+		},
+		{
+			path:'/page',//页面主体
+			component:Page,
+			redirect:'/page/readme',
+			children:[
+				{
+					path:'readme',
+					component:Readme
+				},
+				{
+					path:'resetPwd',
+					component:ResetPwd
+				},
+				{
+					path:'personalCenter',
+					component:PersonalCenter
+				},
+				{
+					path:'table',//表格组件
+					component:Empty,
+					children:[
+						{
+							path:'ele',
+							component:Table
+						},
+						{
+							path:'fixedTable',
+							component:FixedTable
+						},
+						{
+							path:'multistage',
+							component:Multistage
+						},
+						{
+							path:'checkTable',
+							component:CheckTable
+						},
+						{
+							path:'sortTable',
+							component:SortTable
+						},
+						{
+							path:'expandTable',
+							component:ExpandTable
+						},
+						{
+							path:'addUpTable',
+							component:AddUpTable
+						},
+						{
+							path:'mergeTable',
+							component:MergeTable
+						},
+						{
+							path:'elTable',
+							component:ElTable
+						},
+						{
+							path:'dataSource',
+							component:DataSource
+						}
+					]
+				},
+				{
+					path:'else',//其它组件
+					component:Empty,
+					children:[
+						{
+							path:'drag',
+							component:Drag
+						},
+						{
+							path:'tree',
+							component:Tree
+						},{
+							path:"carousel",
+							component:Carousel
+						}
+					]
+				},
+				{
+					path:'chart',//图表组件
+					component:Empty,
+					children:[
+						{
+							path:'sChart',
+							component:Schart
+						}
+					]
+				},
+				{
+					path:'form',//表单组件
+					component:Empty,
+					children:[
+						{
+							path:'baseform',
+							component:Baseform
+						},
+						{
+							path:'extendForm',
+							component:ExtendForm
+						},
+						{
+							path:'registerDone',
+							component:RegisterDone
+						},{
+							path:'formTest',
+							component:FormTest
+						}
+					]
+				},
+				{
+					path:'upload',//上传组件
+					component:Empty,
+					children:[
+						{
+							path:'baseUpload',
+							component:BaseUpload
+						},
+						{
+							path:'picwall',
+							component:Picwall
+						},
+						{
+							path:'piclist',
+							component:Piclist
+						},
+						{
+							path:'dragUpload',
+							component:DragUpload
+						},
+						{
+							path:'clipUpload',
+							component:ClipUpload
+						}
+					]
+				},
+				{
+					path:'tools',//工具组件
+					component:Empty,
+					children:[
+						{
+							path:'tag',
+							component:Tag
+						},
+						{
+							path:'badge',
+							component:Badge
+						},
+						{
+							path:'pagination',
+							component:Pagination
+						},
+						{
+							path:'progress',
+							component:Progress
+						},
+						{
+							path:'step',
+							component:Step
+						}
+					]
+				},
+				{
+					path:'notice',
+					component:Empty,
+					children:[
+						{
+							path:'alert',
+							component:Alert
+						},
+						{
+							path:'message',
+							component:Message
+						},{
+							path:"notification",
+							component:Notification
+						},{
+							path:"loading",
+							component:Loading
+						},{
+							path:"dialog",
+							component:Dialog
+						},{
+							path:'tip',
+							component:Tip
+						},{
+							path:'popover',
+							component:Popover
+						}
+					]
+				},{
+					path:'panel',
+					component:Empty,
+					children:[
+						{
+							path:'collapse',
+							component:Collapse
+						},{
+							path:'card',
+							component:Card
+						}
+					]
+				},{
+					path:'menu',
+					component:Empty,
+					children:[
+						{
+							path:'dropdown',
+							component:Dropdown
+						},{
+							path:'navmenu',
+							component:Navmenu
+						},{
+							path:'tab',
+							component:Tab
+						}
+					]
+				},{
+					path:'layout',
+					component:Empty,
+					redirect:'/page/layout/index',
+					children:[
+						{
+							path:'index',
+							component:Layout
+						},
+						{
+							path:'container',
+							component:Container
+						}
+					]
+				},{
+					path:'overall',
+					component:Empty,
+					children:[
+						{
+							path:'font',
+							component:Font
+						},{
+							path:'color',
+							component:Color
+						},{
+							path:'icon',
+							component:Icon
+						},{
+							path:'button',
+							component:Button
+						}
+					]
+				}
+			]
+		}
+	]
+})
---
+++ src/views/page/Readme.vue
@@ -0,0 +1,82 @@
+<template>
+	<div>
+		<div class="bread-crumb">
+			<el-breadcrumb separator=">>">
+				 <el-breadcrumb-item><i class="el-icon-setting"></i> 自述</el-breadcrumb-item>
+			</el-breadcrumb>
+		</div>
+		<div class="ms-doc">
+            <h3>停车场空位查询系统</h3>
+            <article>
+                <h1>manage-system</h1>
+                <p>基于vue的停车场管理系统</p>
+                <h2>前言</h2>
+                <p>为了解决用户寻找附近停车场不方便的问题，决定设计一个能够迅速查询附近的停车场的信息的系统，给用户提供更好的建议来选择停车场</p>
+
+                <h2>功能</h2>
+              
+                <el-checkbox disabled checked>登录/注销</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>停车场列表显示</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>定位</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>导航</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>搜索</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>支持切换主题色</el-checkbox>
+                <br>
+                <el-checkbox disabled checked>更多</el-checkbox>
+                <br>
+            </article>
+        </div>
+	</div>
+</template>
+
+<style scoped>
+	.ms-doc{
+        width:100%;
+        max-width: 980px;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+    }
+    .ms-doc h3{
+        padding: 9px 10px 10px;
+        margin: 0;
+        font-size: 14px;
+        line-height: 17px;
+        background-color: #f5f5f5;
+        border: 1px solid #d8d8d8;
+        border-bottom: 0;
+        border-radius: 3px 3px 0 0;
+    }
+    .ms-doc article{
+        padding: 45px;
+        word-wrap: break-word;
+        background-color: #fff;
+        border: 1px solid #ddd;
+        border-bottom-right-radius: 3px;
+        border-bottom-left-radius: 3px;
+    }
+    .ms-doc article h1{
+        font-size:32px;
+        padding-bottom: 10px;
+        margin-bottom: 15px;
+        border-bottom: 1px solid #ddd;
+    }
+    .ms-doc article h2 {
+        margin: 24px 0 16px;
+        font-weight: 600;
+        line-height: 1.25;
+        padding-bottom: 7px;
+        font-size: 24px;
+        border-bottom: 1px solid #eee;
+    }
+    .ms-doc article p{
+        margin-bottom: 15px;
+        line-height: 1.5;
+    }
+    .ms-doc article .el-checkbox{
+        margin-bottom: 5px;
+    }
+</style>
